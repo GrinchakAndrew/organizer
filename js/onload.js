39ab08843716d64bfc50dc6338cc3469258a3b18
Andrew John Grinchak <andrewgrinchak@gmail.com>
2016-01-06 16:38:46 +0200

    css

--- js/onload.js
+++ js/onload.js
@@ -3,9 +3,12 @@
 	$(document).ready(function(){
 			var timeout = 0; 
 				timeout = setTimeout(function(){
-					$('.series').first().click();
+					$('.series').first().click(function(){
+						$('.blue-lining').css('display', 'initial');
+						console.log('handler');
+					});
 					clearTimeout(timeout);
-					$('.blue-lining').css('display', 'initial');
+					
 				}, 700);
 		});
 	
