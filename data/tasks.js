3c98021c89f85481b904c717da1e1690baf04ff7
Andrew John Grinchak <andrewgrinchak@gmail.com>
2016-04-21 17:24:29 +0300

    init

--- data/tasks.js
+++ data/tasks.js
@@ -1,28 +1,20 @@
 ﻿{
-	"Miscellany: FED, Backend" : [
-			{"Front-end:" : [ 
-							{"Patterns" : [
-									{"Promise" : {"(1) Promise Pattern" : "data/promise_reworkedCode.txt"}},
-									{"Ellipsize the Limit Text"	: {"Ellipsize overlim" : "data/overlim_Code.txt"}},
-									{"Iframe-2-parent, parent-2-iframe"	: {"Iframe2parent and vice verca" : "data/messages_iframes_violate_same_origin_Code.txt"}}
+	"Welcome to facts about pigs" : [
+			{"ALL ABOUT PIGS:" : [ 
+							
+							{"Interesting Facts" : [
+									{"Fact # 1" : {"Pigs, fact #1" : "data/DocExample1.txt"}}, 
+									{"Fact # 2" : {"Pigs, fact #2" : "data/DocExample2.txt"}}, 
+									{"Fact # 3" : {"Pigs, fact #2" : "data/DocExample3.txt"}}
 								]
-							}, 
-							{"HTML" : [
-									{"Scrollable Menu Solution" : {"(1) Scrollable Menu Solution" : "data/scrollable_menu_nuances_Code.js"}}
-								]
-							}, 
-							{"Selenium" : [
-									{"Iterate Thru WebEls List" : {"(1) WebEls List Iter" : "data/Selenium_Iterate_through_List_webEls_Code.txt"}}
-								]
-							}
+							} 
 				]
 			}
 		],
 	"Photo Gallery" : [
-		{"Nice Pics:" : [
+		{"Nice Pics of Pigs:" : [
 							{"Pics4Fun" : [
-									{"MS Windows Free Gallery" : {"(1) Windows Free Pics" : "data/Induction_agri_repo_pics_trial.txt"}},
-									{"Have Some Pictorial Fun"	: {"(1) Funny Pics" : "data/Induction_agri_repo_pics_trial.txt"}}
+									{"Piglets" : {"(1) Windows Free Pics" : "data/Induction_agri_repo_pics_trial.txt"}}
 								]
 							} 
 				]
